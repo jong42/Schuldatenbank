f415f2850fb1f7ee9da5a4c6b4d2603aba7195c8
Jonas <jonasguetter@gmx.de>
2023-09-13 16:46:39 +0200

    Replace ExecuteReaderAsync by ExecuteReader

--- schuldatenbank/basic_db_functions.cs
+++ schuldatenbank/basic_db_functions.cs
@@ -9,9 +9,10 @@ namespace basic_functions
             var connectionString = "Host=localhost;Username=postgres;Password=mypass;Database=schuldatenbank";
             await using var dataSource = NpgsqlDataSource.Create(connectionString);
             string lehrer = "'" + name + "'," + "'" + vorname + "'," + "'" + geburtsdatum + "'," + "'" + fach1 + "'," + "'" + fach2 + "'," + "'" + fach3 + "'";
-            string sql_command = "INSERT INTO lehrer(name, vorname, geburtsdatum, fach1, fach2, fach3) VALUES(" + lehrer + ")";
+            string sql_command = "INSERT INTO lehrer(name, vorname, geburtsdatum, fach1, fach2, fach3) VALUES(" + lehrer + ");";
+            Console.WriteLine(sql_command);
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
         public async void AddSchüler(string name, string vorname, string geburtsdatum, string klasse)
         {
@@ -20,7 +21,7 @@ namespace basic_functions
             string schüler = "'" + name + "'," + "'" + vorname + "'," + "'" + geburtsdatum + "'," + "'" + klasse + "'";
             string sql_command = "INSERT INTO schüler(name, vorname, geburtsdatum, klasse) VALUES(" + schüler + ")";
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
         public async void AddKurse(string fach, string lehrername, string lehrervorname, string lehrergebdatum, string klasse)
         {
@@ -30,7 +31,7 @@ namespace basic_functions
             string kurs = "'" + kursbezeichnung + "'," + "'" + fach + "'," + "'" + lehrername + "'," + "'" + lehrervorname + "'," + "'" + lehrergebdatum + "'," + "'" + klasse + "'";
             string sql_command = "INSERT INTO kurse(kursbezeichnung, fach, lehrername, lehrervorname, lehrergeburtsdatum, klasse) VALUES(" + kurs + ")";
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
         public async void AddKurstermin(string kursbezeichnung, string wochentag, string uhrzeit)
         {
@@ -39,7 +40,7 @@ namespace basic_functions
             string kurstermin = "'" + kursbezeichnung + "'," + "'" + wochentag +  "'," + "'" + uhrzeit + "'";
             string sql_command = "INSERT INTO kurstermine(kursbezeichnung, wochentag, uhrzeit) VALUES(" + kurstermin + ")";
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
         public async void AddKlausurnote(string kurs, string schülername, string schülervorname, string schülergebdatum, string prüfungsdatum, string note)
         {
@@ -48,7 +49,7 @@ namespace basic_functions
             string klausurnote = "'" + kurs + "'," + "'" + schülername + "'," + "'" + schülervorname + "'," + "'" + schülergebdatum + "'," + "'" + prüfungsdatum + "'," + "'" + note + "'";
             string sql_command = "INSERT INTO klausurnoten (kursbezeichnung, schülername, schülervorname, schülergeburtsdatum, prüfungsdatum, note) VALUES(" + klausurnote + ")";
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
         public async void AddMitarbeitsnote(string kurs, string schülername, string schülervorname, string schülergebdatum, string note)
         {
@@ -57,7 +58,7 @@ namespace basic_functions
             string mitarbeitsnote = "'" + kurs + "'," + "'" + schülername + "'," + "'" + schülervorname + "'," + "'" + schülergebdatum + "'," + "'" + note + "'";
             string sql_command = "INSERT INTO mitarbeitsnoten (kursbezeichnung, schülername, schülervorname, schülergeburtsdatum, note) VALUES(" + mitarbeitsnote + ")";
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
         public async void DeleteLehrer(string name, string vorname, string geburtsdatum)
         {
@@ -65,7 +66,7 @@ namespace basic_functions
             await using var dataSource = NpgsqlDataSource.Create(connectionString);
             string sql_command = "DELETE FROM lehrer WHERE name = " + name + " AND vorname = " + vorname + " AND geburtsdatum = " + geburtsdatum;
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
         public async void DeleteSchüler(string name, string vorname, string geburtsdatum)
         {
@@ -73,7 +74,7 @@ namespace basic_functions
             await using var dataSource = NpgsqlDataSource.Create(connectionString);
             string sql_command = "DELETE FROM schüler WHERE name = " + name + " AND vorname = " + vorname + " AND geburtsdatum = " + geburtsdatum;
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
         public async void DeleteKurse(string kursbezeichnung)
         {
@@ -81,7 +82,7 @@ namespace basic_functions
             await using var dataSource = NpgsqlDataSource.Create(connectionString);
             string sql_command = "DELETE FROM kurse WHERE kursbezeichnung = " + kursbezeichnung;
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
         public async void DeleteKurstermin(string kursbezeichnung, string wochentag, string uhrzeit)
         {
@@ -89,7 +90,7 @@ namespace basic_functions
             await using var dataSource = NpgsqlDataSource.Create(connectionString);
             string sql_command = "DELETE FROM kurstermine WHERE kursbezeichnung = " + kursbezeichnung + " AND wochentag = " + wochentag + " AND uhrzeit = " + uhrzeit;
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
         public async void DeleteKlausurnote(string kursbezeichnung, string schülername, string schülervorname, string schülergeburtsdatum, string prüfungsdatum)
         {
@@ -97,7 +98,7 @@ namespace basic_functions
             await using var dataSource = NpgsqlDataSource.Create(connectionString);
             string sql_command = "DELETE FROM klausurnoten WHERE kursbezeichnung = " + kursbezeichnung + " AND schülername = " + schülername + " AND schülervorname = " + schülervorname + " AND schülergeburtsdatum = " + schülergeburtsdatum + " AND prüfungsdatum = " + prüfungsdatum;
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
         public async void DeleteMitarbeitsnote(string kursbezeichnung, string schülername, string schülervorname, string schülergeburtsdatum)
         {
@@ -105,7 +106,7 @@ namespace basic_functions
             await using var dataSource = NpgsqlDataSource.Create(connectionString);
             string sql_command = "DELETE FROM mitarbeitsnoten WHERE kursbezeichnung = " + kursbezeichnung + " AND schülername = " + schülername + " AND schülervorname = " + schülervorname + " AND schülergeburtsdatum = " + schülergeburtsdatum;
             await using var command = dataSource.CreateCommand(sql_command);
-            await using var reader = await command.ExecuteReaderAsync();
+            await using var reader = command.ExecuteReader();
         }
     }
 }
